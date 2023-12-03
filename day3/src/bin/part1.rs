ebb2a8ca8b8dfe7acc0538a345e1ddc2806d63a6
Tamo <tamo@meilisearch.com>
2023-12-03 14:08:47 +0100

    add day3 part 1

---
+++ day3/src/bin/part1.rs
@@ -0,0 +1,115 @@
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::{self},
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+};
+
+use aoc::*;
+
+#[derive(Debug, Clone)]
+enum Cell {
+    Number(Arc<AtomicUsize>),
+    Symbol(char),
+    Nothing,
+}
+impl Cell {
+    pub fn is_number(&self) -> bool {
+        matches!(self, Cell::Number(_))
+    }
+
+    pub fn is_symbol(&self) -> bool {
+        matches!(self, Cell::Symbol(_))
+    }
+}
+
+impl fmt::Display for Cell {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Cell::Number(n) => write!(f, "{:3?}", n),
+            Cell::Symbol(c) => write!(f, " {c} "),
+            Cell::Nothing => f.write_str(" . "),
+        }
+    }
+}
+
+fn main() {
+    let grid = parser::lines()
+        .map(|line: String| {
+            line.chars()
+                .map(|c| match c {
+                    n @ '0'..='9' => {
+                        let n = n.to_digit(10).unwrap() as usize;
+                        let cell = Arc::new(AtomicUsize::new(n));
+
+                        Cell::Number(cell)
+                    }
+                    '.' => Cell::Nothing,
+                    c if c.is_ascii_punctuation() => Cell::Symbol(c),
+                    c => panic!("What is {c}"),
+                })
+                .collect::<Vec<Cell>>()
+        })
+        .collect::<Vec<_>>();
+
+    let mut grid: Grid<Cell> = Grid::from(grid);
+
+    for coord in Coord::at(0, 0)
+        .to(Coord::at(grid.width() - 1, grid.height() - 1))
+        .unwrap()
+    {
+        if let Some(Cell::Number(prev)) = coord
+            .checked_add(Direction::West)
+            .and_then(|coord| grid.get(coord))
+        {
+            let prev = prev.clone();
+            if let Some(Cell::Number(ref mut cell)) = grid.get_mut(coord) {
+                let current = cell.load(Ordering::Relaxed);
+                *cell = prev;
+                cell.fetch_update(Ordering::Release, Ordering::Acquire, |old| {
+                    Some(old * 10 + current)
+                })
+                .unwrap();
+            }
+        }
+    }
+
+    println!("{grid}");
+
+    let mut seen: HashMap<*mut usize, usize> = HashMap::new();
+
+    for (coord, cell) in grid.enumerate() {
+        if let Cell::Number(n) = cell {
+            // check around
+            for coord in [
+                coord
+                    .checked_add(Direction::North)
+                    .and_then(|coord| coord.checked_add(Direction::West)),
+                coord.checked_add(Direction::North),
+                coord
+                    .checked_add(Direction::North)
+                    .map(|coord| coord + Direction::East),
+                coord.checked_add(Direction::West),
+                Some(coord + Direction::East),
+                coord
+                    .checked_add(Direction::West)
+                    .map(|coord| coord + Direction::South),
+                Some(coord + Direction::South),
+                Some(coord + Direction::South + Direction::East),
+            ]
+            .iter()
+            .filter_map(|coord| *coord)
+            {
+                if let Some(Cell::Symbol(_)) = grid.get(coord) {
+                    seen.insert(n.as_ptr(), n.load(Ordering::Relaxed));
+                }
+            }
+        }
+    }
+
+    let ret: usize = seen.values().sum();
+
+    answer!("{}", ret);
+}

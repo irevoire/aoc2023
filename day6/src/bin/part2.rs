3181ce0f267e5faf01f9851995917ae54f5390cf
Tamo <tamo@meilisearch.com>
2023-12-06 13:21:45 +0100

    add part2

---
+++ day6/src/bin/part2.rs
@@ -0,0 +1,44 @@
+use aoc::{answer, parser};
+use std::str::FromStr;
+
+struct Races(pub (usize, usize));
+
+impl FromStr for Races {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let line = s.lines().collect::<Vec<_>>();
+        let times: usize = line[0]
+            .strip_prefix("Time:")
+            .unwrap()
+            .replace(' ', "")
+            .parse()
+            .unwrap();
+        let distances = line[1]
+            .strip_prefix("Distance:")
+            .unwrap()
+            .replace(' ', "")
+            .parse()
+            .unwrap();
+
+        Ok(Races((times, distances)))
+    }
+}
+
+fn main() {
+    let races = parser::input::<Races>();
+
+    let (duration, record) = races.0;
+    let mut count = 0;
+    for i in 1..duration {
+        if final_distance(i, duration) > record {
+            count += 1;
+        }
+    }
+
+    answer!("{}", count);
+}
+
+fn final_distance(start_after: usize, ends_at: usize) -> usize {
+    (ends_at.saturating_sub(start_after)) * start_after
+}
